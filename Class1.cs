11de7d1972374bd5a7dbda6e91a753b685cf5997
bodnarb.04 <bodnarb.04@A321-13>
2022-03-21 13:10:55 +0100

    zmena

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatumCas04
+{
+    class Class1
+    {
+        public static int PocetDnu(DateTime prodano, int pocetLetZaruky)
+        {
+
+            int zbyva;
+            DateTime dt = DateTime.Now;
+           
+                int rokKoupe = int.Parse(prodano.ToString());
+                int datum = int.Parse(dt.ToString());
+                zbyva = datum - rokKoupe;
+
+            return zbyva;
+
+        }
+    }
+}
